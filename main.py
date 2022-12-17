eea1a84278229846c6d9357c3f9cf25e9e8f45e7
Chris-W-B <survivios198@gmail.com>
2022-12-17 17:54:12 -0600

    Gui work
    More GUI updates

--- main.py
+++ main.py
@@ -2,44 +2,55 @@ import sqlite3
 import tkinter
 # By Team Blue
 
+mainwindow = tkinter.Tk()
+BotomFrame = tkinter.Frame(mainwindow, height=30, width=30)
+TopFrame = tkinter.Frame(mainwindow, height=30, width=30)
+conn = sqlite3.connect('StudentDatabase.db')
+curs = conn.cursor()
 
 def Database():
     Done = False
     Input = False
-    Read = True
-    student_info = []
-    mainwindow=tkinter.Tk()
-    MainFrame=tkinter.Frame(mainwindow, height=30, width=30)
-    info = tkinter.StringVar()
-    x = 1
+    Read = False
+
 # Create connection
-    conn = sqlite3.connect('StudentDatabase.db')
-# Setup Cursor
-    curs = conn.cursor()
+
 # Create a table within the database if it doesn't exist already
 # Using a multiline string simplifies the execute command
-    curs.execute('''CREATE TABLE IF NOT EXISTS StudentDatabase (student_id INTEGER PRIMARY KEY NOT NULL,first_name TEXT NOT NULL,
+    curs.execute('''CREATE TABLE IF NOT EXISTS StudentDatabase(student_id INTEGER PRIMARY KEY NOT NULL,first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL, course_one TEXT NOT NULL, course_two TEXT NOT NULL, hobby_one TEXT NOT NULL,
                 hobby_two TEXT NOT NULL)''')
     curs.execute('''SELECT * From StudentDatabase''')
-    List = str(curs.fetchone())
-    while Done == False:
+    Info = tkinter.Label(TopFrame, text="Student Database. Please select Input to input more data, \nor Read to read existing data")
+    InputButton = tkinter.Button(TopFrame, text="Input")
+    ReadButton = tkinter.Button(TopFrame, text="Read", command=Request)
+    while not Done:
+        print("test")
         if Input == True:
+            print("test")
             Input = False
             conn.commit()
-        if Read == True:
-            label = tkinter.Label(MainFrame, textvariable=info)
-            while List != "None":
-                List += str(curs.fetchone())
-            label.pack()
-            info.set(List)
-            Read = False
         Done=True
-    MainFrame.pack()
-    print("test")
+
+    Info.pack()
+    InputButton.pack()
+    ReadButton.pack()
+    TopFrame.pack()
+    BotomFrame.pack()
     tkinter.mainloop()
     conn.commit()
     conn.close()
+
+def Request():
+    info = tkinter.StringVar()
+    List = str(curs.fetchone())
+    OutLabel = tkinter.Label(BotomFrame, textvariable=info)
+    while List != "None":
+        List += "\n"
+        List += str(curs.fetchone())
+    OutLabel.pack()
+    info.set(List)
+    Read = False
 
 if __name__ == '__main__':
     Database()
