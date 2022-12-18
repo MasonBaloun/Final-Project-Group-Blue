00fe5c0ce9f31fa21f4d06521d8a66e5bef0f536
William-S-M-B <120140527+William-S-M-B@users.noreply.github.com>
2022-12-18 16:01:52 -0600

    added database

--- main.py
+++ main.py
@@ -17,6 +17,27 @@ def Database(): #Creates database and containes first steps of the GUI interface
     curs.execute('''CREATE TABLE IF NOT EXISTS StudentDatabase(student_id INTEGER PRIMARY KEY NOT NULL,first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL, course_one TEXT NOT NULL, course_two TEXT NOT NULL, hobby_one TEXT NOT NULL,
                 hobby_two TEXT NOT NULL)''')
+        #This is the actual database, you guys can just change the ###### to whatever you want
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000001", "William", "Brando", "16", 
+        "MN", "Gaming", "Building Models")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000002", "Christian", "Berry", "########", 
+        "Python Programming", "########", "#######")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000003", "Mason", "Baloun", "########", 
+        "Python Programming", "########", "##########")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000004", "David", "Conway", "17", 
+        "MN", "Art", "Creating Graphic Novels")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000005", "Jacob", "Howler", "16", 
+        "AZ", "Books", "Making Things")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000006", "Yuuji", "Itadori", "16", 
+        "FL", "Watching Anime", "Hanging out with Friends")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000007", "Jay", "Dipersia", "21", 
+        "IL", "Playing Murder Mystery", "Art")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000008", "Ikora", "Rey", "19", 
+        "MD", "Books", "Studying the Stars")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000009", "Isaac", "Nelson", "16", 
+        "CA", "Gaming", "Singing")''')
+    curs.execute('''INSERT INTO TABLE StudentDatabase("00000010", "Alex", "Balken", "22", 
+        "WY", "Welding", "Wood Carving")''')
     curs.execute('''SELECT * From StudentDatabase''')
     Info = tkinter.Label(TopFrame, text="Student Database. Please select Input to input more data, \nor Read to read existing data")
     InputButton = tkinter.Button(TopFrame, text="Input", command=Insert)
